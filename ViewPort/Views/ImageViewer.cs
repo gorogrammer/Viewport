1e97fa2943e2a9f89aff83472ce272bbfa99509b
metalheedo@ajou.ac.kr <metalheedo@ajou.ac.kr>
2020-09-08 20:25:15 +0900

    희도 다시시도

--- ViewPort/Views/ImageViewer.cs
+++ ViewPort/Views/ImageViewer.cs
@@ -119,6 +119,7 @@ namespace ViewPort.Views
                     Current_PageNum = int.Parse(Main.S_Page_TB.Text) + 1;
                     Main.S_Page_TB.Text = Current_PageNum.ToString();
                     Set_Image();
+
             
                 }
             }
